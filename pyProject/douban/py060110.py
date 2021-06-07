653d1ddd3244ac4e8405fff5e0de2fbc608e8100
yang <yungyunbo@gmail.com>
2021-06-07 17:19:24 +0800

    douban

--- pyProject/douban/py060110.py
+++ pyProject/douban/py060110.py
@@ -1,30 +1,45 @@
-# 爬取豆瓣电影 top50
 import re
-
 import requests
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 
+# 获取豆瓣话题评论中的邮箱（视频，提取函数之后）
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'}
 
-response = requests.get('https://movie.douban.com/top250', headers=headers)
-html = response.text
-# print(html)
-'''soup = BeautifulSoup(html, 'lxml')
-movie_ol = soup.find('ol', attrs={'class': 'grid_view'})
-# print(movie_ol)'''
-soup2 = BeautifulSoup(html, 'lxml')
-movie_div = soup2.find_all('div', attrs={'class': 'hd'})
-# print(movie_div)
-# print(type(movie_div))
 
-for i in movie_div:
-    # print(type(i))      #<class 'bs4.element.Tag'>
+def download_page(url):
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-    # movie_title = re.findall(r'class="title">(\w\w\w)</span>', i.text, re.A)
-    movie_title = re.findall(r'<a class="" href="(\w\w\w)">', i.text, re.A)
-    print(movie_title)
+    url_set = set()
+    paginator_eles = soup.find('div', attrs={'class': 'paginator'})
+
+    if paginator_eles:
+        for a_ele in paginator_eles.find_all('a'):
+            url_set.add(a_ele.attrs.get('href'))
+
+        page_obj_list = [soup]
+        for url in url_set:
+            print(f'下载分页{url}')
+            page_obj = requests.get(url, headers=headers)
+            page_obj_url = BeautifulSoup(page_obj.text, 'lxml')
+            page_obj_list.append(page_obj_url)
+    return page_obj_list
 
 
+def fetch_emails(page_obj_list):
+    mail_list = []
+    for page_obj in page_obj_list:
+        comment_eles = page_obj.find_all('div', attrs={'class': 'reply-doc'})
+        for ele in comment_eles:
+            comment_ele = ele.find('p', attrs={'class': 'reply-content'})
+            mail = re.search(r'\w+@\w+.\w+', comment_ele.text, re.A)
+            # print(mail)
+            if mail:
+                pub_time = ele.find('span', attrs={'class': 'pubtime'})
+                print(mail.group(), '\t\t\t', pub_time.text)
 
 
+all_page_list = download_page('https://www.douban.com/group/topic/198233257/')
+fetch_emails(all_page_list)
